ac2763f76482ec8718ebe2f62cedba1c157896cf
Ammo Surfer <ammo.surfer@gmail.com>
2025-06-22 23:35:50 +0000

    "adonan_donut_paha_ayam": {                     "tepung": {"amount": 500

--- src/lib/recipes.ts
+++ src/lib/recipes.ts
@@ -98,24 +98,115 @@ export const recipes: Record<string, Record<string, { amount: number; unit: stri
         "gula_pasir": {"amount": 15, "unit": "grams"},
         "everwhip": {"amount": 167, "unit": "grams"}
     },
-    "adonan_roti_tawar": {
-        "tepung": { "amount": 1000, "unit": "grams" },
-        "gula_pasir": { "amount": 50, "unit": "grams" },
-        "susu_bubuk": { "amount": 40, "unit": "grams" },
-        "ragi": { "amount": 15, "unit": "grams" },
-        "pelembut": { "amount": 5, "unit": "grams" },
-        "garam": { "amount": 15, "unit": "grams" },
-        "margarin": { "amount": 60, "unit": "grams" },
-        "air_dingin": { "amount": 600, "unit": "grams" }
+    "adonan_donat_joko": {
+        "tepung_gerbang": {"amount": 714.2857143, "unit": "grams"},
+        "premix": {"amount": 285.7142857, "unit": "grams"},
+        "gula_pasir": {"amount": 120, "unit": "grams"},
+        "susu_bubuk": {"amount": 50, "unit": "grams"},
+        "ragi": {"amount": 14.28571429, "unit": "grams"},
+        "softer": {"amount": 2.571428571, "unit": "grams"},
+        "telur": {"amount": 2, "unit": "butir"},
+        "margarin": {"amount": 75, "unit": "grams"},
+        "bos": {"amount": 75, "unit": "grams"},
+        "garam": {"amount": 14.28571429, "unit": "grams"},
+        "air": {"amount": 171.42857145, "unit": "grams"},
+        "es_batu": {"amount": 171.42857145, "unit": "grams"}
+    },
+    "adonan_roti_sobek": {
+        "tepung_gerbang": {"amount": 1000, "unit": "grams"},
+        "gula_pasir": {"amount": 200, "unit": "grams"},
+        "telur": {"amount": 1, "unit": "butir"},
+        "susu_bubuk": {"amount": 20, "unit": "grams"},
+        "ragi": {"amount": 13, "unit": "grams"},
+        "pelembut": {"amount": 5, "unit": "grams"},
+        "calsium": {"amount": 4, "unit": "grams"},
+        "ecosoft": {"amount": 35, "unit": "grams"},
+        "margarin": {"amount": 50, "unit": "grams"},
+        "garam": {"amount": 15, "unit": "grams"},
+        "air": {"amount": 300, "unit": "grams"},
+        "es_batu": {"amount": 150, "unit": "grams"}
+    },
+    "adonan_roti_sobek_coklat": {
+        "tepung_gerbang": {"amount": 1040, "unit": "grams"},
+        "gula_pasir": {"amount": 260, "unit": "grams"},
+        "telur": {"amount": 1, "unit": "butir"},
+        "coklat_bubuk": {"amount": 60, "unit": "grams"},
+        "ragi": {"amount": 23, "unit": "grams"},
+        "pelembut": {"amount": 4, "unit": "grams"},
+        "calsium": {"amount": 2, "unit": "grams"},
+        "uht": {"amount": 200, "unit": "grams"},
+        "margarin": {"amount": 160, "unit": "grams"},
+        "garam": {"amount": 8, "unit": "grams"},
+        "air": {"amount": 200, "unit": "grams"},
+        "es_batu": {"amount": 150, "unit": "grams"}
+    },
+    "adonan_butter_balok_mocha": {
+        "margarin": {"amount": 6000, "unit": "grams"},
+        "gula_halus": {"amount": 2000, "unit": "grams"},
+        "skm": {"amount": 1000, "unit": "grams"},
+        "coklat_filling": {"amount": 1000, "unit": "grams"}
+    },
+    "adonan_brownies": {
+        "tepung_brownies": {"amount": 366.6666667, "unit": "grams"},
+        "telur_ayam": {"amount": 1.666666667, "unit": "butir"},
+        "minyak": {"amount": 208.3333333, "unit": "grams"},
+        "air": {"amount": 45.83333333, "unit": "grams"}
+    },
+    "adonan_banana_cake": {
+        "tepung_segitiga": {"amount": 281.25, "unit": "grams"},
+        "pisang": {"amount": 281.25, "unit": "grams"},
+        "gula_pasir": {"amount": 281.25, "unit": "grams"},
+        "baking_powder": {"amount": 5, "unit": "grams"},
+        "soda_kue": {"amount": 5, "unit": "grams"},
+        "margarin": {"amount": 87.5, "unit": "grams"},
+        "minyak": {"amount": 62.5, "unit": "grams"},
+        "telur_ayam": {"amount": 3, "unit": "butir"},
+        "uht": {"amount": 125, "unit": "grams"}
+    },
+    "adonan_banana_cake_pandan": {
+        "tepung_segitiga": {"amount": 281.25, "unit": "grams"},
+        "pisang": {"amount": 281.25, "unit": "grams"},
+        "gula_pasir": {"amount": 281.25, "unit": "grams"},
+        "baking_powder": {"amount": 5, "unit": "grams"},
+        "soda_kue": {"amount": 5, "unit": "grams"},
+        "margarin": {"amount": 87.5, "unit": "grams"},
+        "minyak": {"amount": 62.5, "unit": "grams"},
+        "telur_ayam": {"amount": 3, "unit": "butir"},
+        "uht": {"amount": 125, "unit": "grams"},
+        "pasta_pandan": {"amount": 10, "unit": "grams"}
+    },
+    "adonan_chiffon": {
+        "tepung_chiffon_premix": {"amount": 142.8571429, "unit": "grams"},
+        "telur_ayam": {"amount": 3.142857143, "unit": "butir"},
+        "minyak": {"amount": 7.142857143, "unit": "grams"},
+        "pasta_pandan_mocha": {"amount": 4.285714286, "unit": "grams"},
+        "air": {"amount": 7.142857143, "unit": "grams"}
+    },
+    "adonan_bolu_gulung": {
+        "tepung_segitiga": {"amount": 150, "unit": "grams"},
+        "gula_pasir": {"amount": 150, "unit": "grams"},
+        "sp": {"amount": 37.5, "unit": "grams"},
+        "susu_bubuk": {"amount": 25, "unit": "grams"},
+        "maizena": {"amount": 25, "unit": "grams"},
+        "telur_ayam": {"amount": 10, "unit": "butir"},
+        "margarin": {"amount": 150, "unit": "grams"}
     },
     "adonan_muffin": {
-        "tepung": { "amount": 500, "unit": "grams" },
-        "gula_pasir": { "amount": 250, "unit": "grams" },
-        "baking_powder": { "amount": 20, "unit": "grams" },
-        "garam": { "amount": 5, "unit": "grams" },
-        "telur": { "amount": 2, "unit": "butir" },
-        "susu_cair": { "amount": 250, "unit": "ml" },
-        "minyak_sayur": { "amount": 125, "unit": "ml" },
-        "ekstrak_vanila": { "amount": 5, "unit": "ml" }
+        "tepung_muffin": {"amount": 38.67924528, "unit": "grams"},
+        "telur_ayam": {"amount": 0.301886792, "unit": "butir"},
+        "minyak": {"amount": 15.28301887, "unit": "grams"},
+        "air": {"amount": 15.28301887, "unit": "grams"}
+    },
+    "adonan_roti_tawar": {
+        "tepung_gerbang": { "amount": 1000, "unit": "grams" },
+        "gula_pasir": { "amount": 120, "unit": "grams" },
+        "susu_bubuk": { "amount": 30, "unit": "grams" },
+        "s500": { "amount": 10, "unit": "grams" },
+        "garam": { "amount": 15, "unit": "grams" },
+        "butter": { "amount": 100, "unit": "grams" },
+        "ragi": { "amount": 15, "unit": "grams" },
+        "pelembut": { "amount": 5, "unit": "grams" },
+        "es_batu": { "amount": 150, "unit": "grams" },
+        "air": { "amount": 300, "unit": "grams" }
     }
 };
